eb55dff7c00afeb6b673c8abf20ea7f054598ece
Frederik <frederikblockchain@gmail.com>
2023-04-11 09:57:52 +0200

    algokit repo

---
+++ smart_contracts/code/eventticket.py
@@ -0,0 +1,68 @@
+from typing import Literal
+
+import beaker as bk
+from pyteal import (
+    Expr,
+    Global,
+    InnerTxnBuilder,
+    Int,
+    Seq,
+    Txn,
+    TxnField,
+    TxnType,
+    abi,
+)
+
+app = bk.Application("EventTicket")
+
+
+@app.external
+def create_asset(
+    assetName: abi.String, assetUrl: abi.String, assetTotal: abi.Uint64, metadataHash: abi.StaticBytes[Literal[32]]
+) -> Expr:
+    return Seq(  # Seq is used to group a set of operations with only the last returning a value on the stack
+        # Start to build the transaction builder
+        InnerTxnBuilder.Begin(),
+        # This method accepts a dictionary of TxnField to value so all fields may be set
+        InnerTxnBuilder.SetFields(
+            {
+                TxnField.type_enum: TxnType.AssetConfig,
+                TxnField.config_asset_name: assetName.get(),
+                TxnField.config_asset_url: assetUrl.get(),
+                TxnField.config_asset_manager: Global.current_application_address(),
+                TxnField.config_asset_clawback: Global.current_application_address(),
+                TxnField.config_asset_reserve: Global.current_application_address(),
+                TxnField.config_asset_freeze: Global.current_application_address(),
+                TxnField.config_asset_total: assetTotal.get(),
+                TxnField.config_asset_metadata_hash: metadataHash.get(),
+                TxnField.config_asset_decimals: Int(0),
+            }
+        ),
+        # Submit the transaction we just built
+        InnerTxnBuilder.Submit(),
+    )
+
+
+@app.external
+def get_asset(asset: abi.Asset) -> Expr:
+    return Seq(  # Seq is used to group a set of operations with only the last returning a value on the stack
+        # Start to build the transaction builder
+        InnerTxnBuilder.Begin(),
+        # This method accepts a dictionary of TxnField to value so all fields may be set
+        InnerTxnBuilder.SetFields(
+            {
+                TxnField.type_enum: TxnType.AssetTransfer,
+                TxnField.xfer_asset: asset.asset_id(),
+                TxnField.asset_amount: Int(1),
+                TxnField.asset_receiver: Txn.sender(),
+                TxnField.asset_sender: Global.current_application_address(),
+            }
+        ),
+        # Submit the transaction we just built
+        InnerTxnBuilder.Submit(),
+    )
+
+
+if __name__ == "__main__":
+    spec = app.build()
+    spec.export("artifacts")
--- smart_contracts/deploy-config.ts
+++ smart_contracts/deploy-config.ts
@@ -2,7 +2,7 @@ import * as algokit from '@algorandfoundation/algokit-utils'
 import { AppSpec } from '@algorandfoundation/algokit-utils/types/appspec'
 
 // Edit this to add in your contracts
-export const contracts = ['HelloWorldApp'] as const
+export const contracts = ['HelloWorldApp', 'calculator.py'] as const
 
 export async function deploy(name: (typeof contracts)[number], appSpec: AppSpec) {
   const algod = algokit.getAlgoClient()
@@ -27,36 +27,30 @@ export async function deploy(name: (typeof contracts)[number], appSpec: AppSpec)
     },
     algod,
   )
+  const app = await appClient.deploy({
+    allowDelete: isLocal,
+    allowUpdate: isLocal,
+    onSchemaBreak: isLocal ? 'replace' : 'fail',
+    onUpdate: isLocal ? 'update' : 'fail',
+  })
+  // If app was just created fund the app account
+  if (['create', 'replace'].includes(app.operationPerformed)) {
+    algokit.transferAlgos(
+      {
+        amount: algokit.algos(1),
+        from: deployer,
+        to: app.appAddress,
+      },
+      algod,
+    )
+  }
+}
 
-  switch (name) {
-    // Edit this to add the custom deployment logic for each contract
-    case 'HelloWorldApp':
-      const app = await appClient.deploy({
-        allowDelete: isLocal,
-        allowUpdate: isLocal,
-        onSchemaBreak: isLocal ? 'replace' : 'fail',
-        onUpdate: isLocal ? 'update' : 'fail',
-      })
-      // If app was just created fund the app account
-      if (['create', 'replace'].includes(app.operationPerformed)) {
-        algokit.transferAlgos(
-          {
-            amount: algokit.algos(1),
-            from: deployer,
-            to: app.appAddress,
-          },
-          algod,
-        )
-      }
-
-      const method = 'hello'
+/*
+const method = 'hello'
       const methodArgs = ['world']
       const response = await appClient.call({ method: method, methodArgs: methodArgs })
       console.log(
         `Called ${method} on ${name} (${app.appId}) with args=[${methodArgs}], received: ${response.return?.returnValue}`,
       )
-      break
-    default:
-      throw new Error(`Attempt to deploy unknown contract ${name}`)
-  }
-}
+*/
